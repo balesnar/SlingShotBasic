a5319022ffac88e9b5676aee0656e9ebc6815448
Brenna Lesnar <lesnarb0041@my.uwstout.edu>
2015-07-16 16:49:12 +0200

    Tree Removal
    Took Out Tree

---
+++ Assets/Collide1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collide1 : MonoBehaviour
+
+	{
+
+	// Use this for initialization
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "trigger")
+		{
+			Destroy(col.gameObject);
+	
+	}
+	
+ 
+	
+	}
+}
